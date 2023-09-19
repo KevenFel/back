84f92d55658aeef65a3611ec823b6690dea45fde
Keven <kevenludson@gmail.com>
2023-09-18 18:25:07 -0600

    cambios en el db y gitignore

--- back/src/db.js
+++ back/src/db.js
@@ -3,11 +3,8 @@ import cliente from 'pg';
 const { Pool } = cliente;
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crud_uno',
-    password: 'lukefe509@',
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 export {
